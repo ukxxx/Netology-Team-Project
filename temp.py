98203e37d8aacac737c6e753e5e786ec4f2ceeb9
ukxxx <42536544+ukxxx@users.noreply.github.com>
2023-07-12 14:28:24 +0300

    changes in temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,10 @@
-import os
-import asyncio
 import aiofiles
 import json
 
 
-async def read_file(file_path):
+async def read_token(file_path):
     async with aiofiles.open(file_path, mode='r') as file:
         data = await file.read()
         data = json.loads(data)
-        return data
-    
-token = asyncio.run(read_file('vk_credentials.json'))['token']
-print(result['token'])
+        token = data['token']
+        return token
