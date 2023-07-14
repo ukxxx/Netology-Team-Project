7f853b64ce8955519c73a3df4a1207da60cc44eb
ukxxx <42536544+ukxxx@users.noreply.github.com>
2023-07-14 12:29:57 +0300

    All async trying moved to different files and added to gitignore

--- vk_interaction.py
+++ vk_interaction.py
@@ -83,10 +83,10 @@ class VkSaver:
         return result
 
 
-# temp = VkSaver('vk1.a.7kq5ikN3cbvq844t_GN_lkGOBfp1bhByb8Tp9MT2vgVVkbs_6fiUj-bDGUfL-A74cY8wK0yx1xnBro-Hg6n9t5x3bpDE9fKglPQzxdtA2U0Qn3DnFwEuBzegVxhv0Iznku8p_p5eseAImcMLqYbSi68kQTDoa6VEqddVqd_vu6F-1mEB3UJPBWAAavcHD8g86yeTaVnr61Uer_H9bGqepA')
-# user_list = temp.get_user_list(1, 1, 25, 40, 1)
+temp = VkSaver('vk1.a.7kq5ikN3cbvq844t_GN_lkGOBfp1bhByb8Tp9MT2vgVVkbs_6fiUj-bDGUfL-A74cY8wK0yx1xnBro-Hg6n9t5x3bpDE9fKglPQzxdtA2U0Qn3DnFwEuBzegVxhv0Iznku8p_p5eseAImcMLqYbSi68kQTDoa6VEqddVqd_vu6F-1mEB3UJPBWAAavcHD8g86yeTaVnr61Uer_H9bGqepA')
+user_list = temp.get_user_list(1, 1, 25, 40, 1)
 # pprint(user_list)
-# album_ids = temp.get_list_of_album_ids(5, 0)
+album_ids = temp.get_list_of_album_ids(5, 0)
 # print(album_ids)
-# photos_list = temp.get_toprated_photos(album_ids[0])
-# pprint(photos_list)
+photos_list = temp.get_toprated_photos(album_ids[0])
+pprint(photos_list)
