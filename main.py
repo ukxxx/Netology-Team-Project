1a7d728fd8acc1d69acd18769be35a82e26e9726
ukxxx <42536544+ukxxx@users.noreply.github.com>
2023-07-14 11:28:44 +0300

    Added asynchronous token reader

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from vkbottle.bot import Bot, Message
 from vkbottle import Keyboard, Text, KeyboardButtonColor
+import asyncio
 
 from vk_interaction import VkSaver
+from async_read_token import *
 
 
-bot = Bot(
-    token="vk1.a.CLld4ad1X90Em_kENKKCz_6X7F-FgqpQIdPjTfO0yobWtgp7dNFxNdeCAEb_TOQGMaPtuFShfTOA5XzB17mIx6U5MjHlJBrBuo7-nsobfUoTouCqt3shWw3E9nsmR0E2b1mngQRfm0Vk5cwgzJntVc7a_7xHYh1AvsXTKILYDPQBxHuwpGs13fsk_WHg9jsn4kRpGhnOtdfHHizfXdh81A"
-)
+group_token, personal_token = asyncio.run(get_token_async())
+bot = Bot(token=group_token)
+# bot = Bot(token="vk1.a.CLld4ad1X90Em_kENKKCz_6X7F-FgqpQIdPjTfO0yobWtgp7dNFxNdeCAEb_TOQGMaPtuFShfTOA5XzB17mIx6U5MjHlJBrBuo7-nsobfUoTouCqt3shWw3E9nsmR0E2b1mngQRfm0Vk5cwgzJntVc7a_7xHYh1AvsXTKILYDPQBxHuwpGs13fsk_WHg9jsn4kRpGhnOtdfHHizfXdh81A")
+
 keyboard = (
     Keyboard(one_time=False, inline=False)
     .add(Text("💔 Дальше"))
@@ -45,4 +48,4 @@ async def hi_handler(message: Message):
     )
 
 
-bot.run_forever()
+asyncio.run(bot.run_polling())
