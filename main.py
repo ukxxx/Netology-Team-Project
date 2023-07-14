f335a51e846795dadc22af93dd7e19cf6aba9655
Pavel Zavartsev <pavel_zavartsev@mail.ru>
2023-07-15 00:04:07 +0300

    Add func count_age, send_photo, set_params_to_match, get_user_data, connection to longpoll

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import json
 import logging
 from random import randrange
-
 import vk_api
 from vk_api.keyboard import VkKeyboard
 from vk_api.longpoll import VkLongPoll, VkEventType
-
+import requests
 from vk_interaction import VkSaver
+from datetime import date
+today = date.today()
+
+
+def count_age(bdate):
+
+    if len(bdate) > 5:
+        day = int(bdate[:2])
+        month = int(bdate[3:5])
+        year = int(bdate[6:10])
+        age = today.year - year - ((today.month, today.day) < (month, day))
+        return age
+    else:
+        print("не указан год рождения")
+        return None
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 with open('vk_credentials.json', 'r') as file:
     token = json.loads(file.read())['group_token']
-print(token)
+with open('vk_credentials.json', 'r') as file:
+    p_token = json.loads(file.read())['personal_token']
+
 vk = vk_api.VkApi(token=token)
+vksaver = VkSaver(p_token)
+
 longpoll = VkLongPoll(vk)
+res = vk.method('messages.getLongPollServer')
+try:
+    connect = requests.get(f"https://{res['server']}?act=a_check&key={res['key']}&ts={res['ts']}&wait=25&mode=2&version=3")
+    if connect.status_code == 200:
+        print('Соединение с ботом установлено')
+except Exception as ex:
+    print(ex)
+
 
 keyboard = VkKeyboard(one_time=False, inline=False)
+keyboard.add_button("💓 Начать 💓")
 keyboard.add_button("💔 Дальше")
 keyboard.add_button("❤ Сохранить в избранном")
 keyboard.add_line()
@@ -29,17 +56,43 @@ def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7), 'keyboard': keyboard})
 
 
+def send_photo(user_id, photo_url):
+    vk.method('messages.send', {'user_id': user_id, 'attachment': f"photo{user_id}_{photo_url}", 'random_id': randrange(10 ** 7), 'keyboard': keyboard})  # допилить
+
+
+def set_params_to_match(user):
+    params_to_match = {
+        "city": user["city"]["id"],
+        "sex": user["sex"],
+        "age_from": count_age(user["bdate"]),
+        "age_to": count_age(user["bdate"])
+    }
+    return params_to_match
+
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
 
         if event.to_me:
             request = event.text
 
-            if request == "💔 Дальше":
+            if request == "💓 Начать 💓":
+                write_msg(event.user_id, f"НАЧНЕМ")
+                vksaver.get_user_data(event.user_id)
+
+            elif request == "💔 Дальше":
                 write_msg(event.user_id, f"ТУТ_БУДЕТ_НОВЫЙ_ЧЕЛОВЕК")
+                user = vksaver.get_user_data(event.user_id)
+                params = set_params_to_match(user)
+                ids = vksaver.get_user_list(**params)
+                print(ids)
+                albums_id = vksaver.get_list_of_album_ids(ids[0]['id'])
+                top_photos = vksaver.get_toprated_photos(albums_id)
+                send_photo(event.user_id, top_photos)
             elif request == "😍 Избранное":
                 write_msg(event.user_id, f"ТУТ_БУДЕТ_ИЗБРАННОЕ")
             elif request == "❤ Сохранить в избранном":
                 write_msg(event.user_id, f"Сохранен в избранном")
             else:
                 write_msg(event.user_id, f"Не понял вашего ответа...")
+                
--- vk_interaction.py
+++ vk_interaction.py
@@ -8,8 +8,23 @@ class VkSaver:
     def __init__(self, token: str, version="5.131"):
         self.params = {"access_token": token, "v": version}
 
+    def get_user_data(
+            self, user_id
+    ):
+        get_user_data = self.url + "users.get"
+        user_params = {
+            "user_ids": user_id,
+            "fields": "city, sex, bdate"
+        }
+
+        user = requests.get(
+            get_user_data, params={**self.params, **user_params}
+        ).json()
+
+        return user["response"][0]
+
     def get_user_list(
-        self, city, sex, age_from, age_to, offset
+        self, city, sex, age_from, age_to
     ):  # Принимает параметры для поиска и возвращает список словарей с данными пользователей
         get_user_id_url = self.url + "users.search"
         user_params = {
@@ -17,7 +32,9 @@ class VkSaver:
             "sex": sex,
             "age_from": age_from,
             "age_to": age_to,
-            "offset": offset,
+            "has_photo": 1,
+            "count": 100,
+            "offset": 20
         }
         result = requests.get(
             get_user_id_url, params={**self.params, **user_params}
@@ -29,11 +46,11 @@ class VkSaver:
             return result["response"]["items"]
 
     def get_list_of_album_ids(
-        self, owner_id, offset
+        self, owner_id
     ):  # Принимает айди пользователя и возвращает список его альбомов
         self.owner_id = owner_id
         get_album_url = self.url + "photos.getAlbums"
-        album_params = {"owner_id": owner_id, "offset": offset, "need_system": 1}
+        album_params = {"owner_id": owner_id, "offset": 1, "need_system": 1}
         result = requests.get(
             get_album_url, params={**self.params, **album_params}
         ).json()
@@ -83,10 +100,13 @@ class VkSaver:
         return result
 
 
-temp = VkSaver('vk1.a.7kq5ikN3cbvq844t_GN_lkGOBfp1bhByb8Tp9MT2vgVVkbs_6fiUj-bDGUfL-A74cY8wK0yx1xnBro-Hg6n9t5x3bpDE9fKglPQzxdtA2U0Qn3DnFwEuBzegVxhv0Iznku8p_p5eseAImcMLqYbSi68kQTDoa6VEqddVqd_vu6F-1mEB3UJPBWAAavcHD8g86yeTaVnr61Uer_H9bGqepA')
-user_list = temp.get_user_list(1, 1, 25, 40, 1)
-# pprint(user_list)
-album_ids = temp.get_list_of_album_ids(5, 0)
-# print(album_ids)
-photos_list = temp.get_toprated_photos(album_ids[0])
-pprint(photos_list)
+# if __name__ == "__main__":
+
+#
+# temp = VkSaver('vk1.a.7kq5ikN3cbvq844t_GN_lkGOBfp1bhByb8Tp9MT2vgVVkbs_6fiUj-bDGUfL-A74cY8wK0yx1xnBro-Hg6n9t5x3bpDE9fKglPQzxdtA2U0Qn3DnFwEuBzegVxhv0Iznku8p_p5eseAImcMLqYbSi68kQTDoa6VEqddVqd_vu6F-1mEB3UJPBWAAavcHD8g86yeTaVnr61Uer_H9bGqepA')
+# user_list = temp.get_user_list(1, 1, 25, 40)
+# # pprint(user_list)
+# album_ids = temp.get_list_of_album_ids(5)
+# # print(album_ids)
+# photos_list = temp.get_toprated_photos(album_ids[0])
+# pprint(photos_list)
