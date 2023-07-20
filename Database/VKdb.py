7b174f027ac7ce82c153316b7ef1d084d7f96bdc
Pavel Zavartsev <pavel_zavartsev@mail.ru>
2023-07-20 22:56:41 +0300

    working add favourite

--- Database/VKdb.py
+++ Database/VKdb.py
@@ -48,26 +48,25 @@ class VKDataBase:
         )
         self.session.add(user)
         self.session.commit()
-        self.session.close()
+        return user
 
-    def save_photo(self, user_id, photo):
-        photo = Photo(user_id=user_id, photo=photo)
+    def save_photo(self, user, photo):
+        photo = Photo(vk_id=user.vk_id, photo=photo)
         self.session.add(photo)
         self.session.commit()
-        self.session.close()
+        return photo
 
-    def save_match(self, user_id, user2_id):
-        match = Match(user_id=user_id, user2_id=user2_id)
-        print(match) # принтуя, возвращает None вместо match_id, а он нам нужен, чтоб сохранить в таблицу favorites
+    def save_match(self, user, user2):
+        match = Match(vk_id=user.vk_id, user_id=user2.vk_id)
         self.session.add(match)
         self.session.commit()
-        self.session.close()
+        return match
 
-    def add_to_favourite(self, match_id):
-        favourite = Favourite(match_id=match_id)
+    def add_to_favourite(self, match):
+        favourite = Favourite(match_id=match.match_id)
         self.session.add(favourite)
         self.session.commit()
-        self.session.close()
+        return favourite
 
     def delete(self):
         self.session.query(Photo).delete()
@@ -77,6 +76,28 @@ class VKDataBase:
         self.session.commit()
         self.session.close()
 
+
+    def query_user_id(self, vk_id):
+        user = self.session.query(User).filter(User.vk_id==vk_id)
+
+        for i in user:
+            u_id = i.vk_id
+
+        return u_id
+
+    def query_match_id(self, user_id):
+        # match = self.session.query(User.vk_id, Match.match_id).\
+        #     join(User).join(Match).filter(User.vk_id == user_id)
+        match = self.session.query(Match.match_id, Match.vk_id, Match.user_id).filter(Match.vk_id == user_id).one()
+        print(type(match))
+        print(match._mapping)
+        # favourite = match.match_id
+        #
+        # for match_id in match:
+        #     i = match_id.match_id
+        # print(i)
+        # print(favourite)
+        return match
 
 # if __name__ == "__main__":
 #     vk_db = VKDataBase()
--- Database/models.py
+++ Database/models.py
@@ -8,44 +8,44 @@ class User(Base):
     __tablename__ = "users"
 
     # user_id = sq.Column(sq.BigInteger, primary_key=True)
-    vk_id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.BigInteger, primary_key=True)
     first_name = sq.Column(sq.String(length=100), nullable=False)
     last_name = sq.Column(sq.String(length=100), nullable=False)
     age = sq.Column(
         sq.Integer, nullable=False
     )  
     sex = sq.Column(
-        sq.String, nullable=False
+        sq.Integer, nullable=False
     )  
     __table_args__ = (
         sq.CheckConstraint("sex IN ('1', '2')", name='check_sex_value'),
         sq.CheckConstraint('age >= 0 AND age <= 999', name='check_age_length'),
     )
-    city = sq.Column(sq.String, nullable=False)
+    city = sq.Column(sq.Integer, nullable=False)
 
     def __str__(self):
-        return f"User {self.vk_id}: ({self.name}, {self.age}, {self.gender}, {self.city})"
+        return f"User {self.vk_id}: ({self.first_name}, {self.last_name}, {self.age}, {self.sex}, {self.city})"
 
 
 class Match(Base):
     __tablename__ = "matches"
 
     match_id = sq.Column(sq.BigInteger, primary_key=True)
+    vk_id = sq.Column(sq.BigInteger, sq.ForeignKey("users.vk_id"), nullable=False)
     user_id = sq.Column(sq.BigInteger, sq.ForeignKey("users.vk_id"), nullable=False)
-    user2_id = sq.Column(sq.BigInteger, sq.ForeignKey("users.vk_id"), nullable=False)
 
-    us = relationship(User, foreign_keys=[user_id])
-    us2 = relationship(User, foreign_keys=[user2_id])
+    us = relationship(User, foreign_keys=[vk_id])
+    us2 = relationship(User, foreign_keys=[user_id])
 
     def __str__(self):
-        return f"Match {self.match_id}: ({self.user_id}, {self.user2_id})"
+        return f"Match {self.match_id}: ({self.vk_id}, {self.user_id})"
 
 
 class Photo(Base):
     __tablename__ = "photos"
 
     photo_id = sq.Column(sq.BigInteger, primary_key=True)
-    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("users.vk_id"), nullable=False)
+    vk_id = sq.Column(sq.BigInteger, sq.ForeignKey("users.vk_id"), nullable=False)
     photo = sq.Column(
         sq.String
     )  # здесь ссылка, поискать конкретно для url другой метод
@@ -55,7 +55,7 @@ class Photo(Base):
 
     def __str__(self):
         return (
-            f"Photo {self.photo_id}: ({self.user_id}, {self.photo}, {self.photo_likes})"
+            f"Photo {self.photo_id}: ({self.vk_id}, {self.photo})"
         )
 
 
@@ -74,5 +74,5 @@ class Favourite(Base):
 
 
 def create_tables(engine):
-    Base.metadata.drop_all(engine)
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
